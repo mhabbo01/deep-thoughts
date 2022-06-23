e6e75a514776f2176329bd3dccd56e8b284aba7d
Matt Habbo <matthabbo45@gmail.com>
2022-06-23 12:58:11 -0400

    set up react client side, concurrently root package.json

---
+++ client/src/utils/queries.js
@@ -0,0 +1,18 @@
+import { gql } from '@apollo/client';
+
+export const QUERY_THOUGHTS = gql`
+    query thoughts($username: String) {
+        thoughts(username: $username) {
+            _id
+            thoughtText
+            username
+            reactionCount
+            reactions {
+                _id
+                createdAt
+                username
+                reactionBody
+            }
+        }
+    }
+`;
--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const { ApolloServer } = require('apollo-server-express');
 const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require('./utils/auth');
 const db = require('./config/connection');
+const path = require('path');
 
 const PORT = process.env.PORT || 3001;
 // create new apollo server and pass it our schema data
@@ -19,11 +20,20 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+};
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
 //create a new instance of an Apollo server with graphQL schema
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
   // integrate our apollo server with the express application as middleware
   server.applyMiddleware({ app });
+
 
   db.once('open', () => {
     app.listen(PORT, () => {
